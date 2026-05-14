518bf24d99e2eb883440986280c1760b47baa0ca
azhar.uddin@studectech.com <azhar.uddin@studectech.com>
2026-05-14 18:03:51 +0530

    learned leetcode recursion

---
+++ take_u_forward_dsa/src/take_u_forward_dsa/recursion/ResursionA.java
@@ -0,0 +1,16 @@
+package take_u_forward_dsa.recursion;
+
+public class ResursionA {
+	public static void main(String[] args) {
+		printName();
+	}
+	
+	static int i =0;
+	public static int printName() {
+		if( i == 20) return 10;
+		i++;
+		System.out.println("Azhar printed");
+		 return printName();
+	}
+
+}
