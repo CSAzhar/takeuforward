bc9e07138196beaf39b850b8ac96050721d0dec8
azhar.uddin@studectech.com <azhar.uddin@studectech.com>
2026-07-06 12:46:58 +0530

    added

--- take_u_forward_dsa/src/take_u_forward_dsa/array/MajorityElement2.java
+++ take_u_forward_dsa/src/take_u_forward_dsa/array/MajorityElement2.java
@@ -15,17 +15,33 @@ class Solution {
         for(int i=0; i<nums.length; i++){
             map.put(nums[i], map.getOrDefault(nums[i], 0)+1);
         }
-//        List<Integer> ans = new ArrayList<>();
-//        for(Integer key: map.keySet()){
-//            if(map.get(key)> nums.length/3){
-//                ans.add(key);
-//            }
-//        }
+
         List<Integer> ans2 = map.entrySet()
         									.stream()
         									.filter(e -> e.getValue() > (nums.length/3))
         									.map(e ->e.getKey())
         									.collect(Collectors.toList());
         return ans2;
+    }
+    
+    public List<Integer> maorityOptimized(int[] nums){
+    	int count1=0, ans1=0, count2=0, ans2=0;
+    	for(int i=0; i<nums.length; i++) {
+    		if(count1 == 0) {
+    			ans1 = nums[i]; count1++;
+    		}else if(count2 == 0) {
+    			ans2 = nums[i]; count2++;
+    		}else if(nums[i] == ans1 && nums[i] != ans2) {
+    			count1++;
+    		}
+    		else if(nums[i] == ans2 && nums[i] != ans1) {
+    			count2++;
+    		}else {
+    			count1--; count2--;
+    		}
+    	}
+    	List<Integer> ans = new ArrayList<>();
+    	ans.add(ans1, ans2);
+    	return ans;
     }
 }
