5d245800c1012284fd8ed84fc43532ed6ffe42c6
CSAzhar <azhardarampur@gmail.com>
2026-07-06 22:37:40 +0530

    added

--- take_u_forward_dsa/src/take_u_forward_dsa/recursion/SumOfFirstNNumbers.java
+++ take_u_forward_dsa/src/take_u_forward_dsa/recursion/SumOfFirstNNumbers.java
@@ -5,9 +5,12 @@ import java.util.Arrays;
 public class SumOfFirstNNumbers {
 	
 	public static void main(String[] args) {
-		String[] arr = {"p", "a", "b", "k", "a"};
-		reverseString(arr, 0, arr.length-1);
-		System.out.println(Arrays.toString(arr));
+//		String[] arr = {"p", "a", "b", "k", "a"};
+//		reverseString(arr, 0, arr.length-1);
+//		System.out.println(Arrays.toString(arr));
+		String str = "abcdeedba";
+//		System.out.println(checkPalindrome(str, 0, str.length()-1));
+		System.out.println(checkPrime(2, 7));
 	}
 	
 	public static int sumOfFirstN(int n) {
@@ -31,6 +34,18 @@ public class SumOfFirstNNumbers {
 		arr[left] = arr[right];
 		arr[right] = leftString;
 		reverseString(arr, left+1, right-1);
+	}
+	
+	public static boolean checkPalindrome(String str, int left, int right) {
+		if(left>=right) return true;
+		if (str.charAt(left)!= str.charAt(right)) return false;
+		return checkPalindrome(str, left+1, right-1);
+	}
+	
+	public static boolean checkPrime(int i, int n) {
+		if(i>=n) return true;
+		if(n % i == 0) return false;
+		return checkPrime(i+1, n);
 	}
 
 }
